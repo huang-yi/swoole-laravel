5a2b345bbe0bd59f6d349a63a168123145db3cbe
huangyi <coodeer@163.com>
2017-02-09 02:16:12 +0800

    SwooleServiceProvider

---
+++ src/SwooleServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\Swoole;
+
+use Illuminate\Support\ServiceProvider;
+
+class SwooleServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+}
